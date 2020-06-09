f88464ac0031dc0e73b1cfa19a998da309b60a58
Jackson Bierfeldt <jbierfeldt@gmail.com>
2020-06-09 00:08:55 -0400

    Improved Dev Environment and made MVP for P5js Graphics Engine

--- client.js
+++ client.js
@@ -1,16 +1,40 @@
-import p5 from 'p5';
-// import Unit from './lib/shared/Unit.js';
+// import p5 from 'p5';
+import Game from './lib/shared/Game.js';
+import Display from './lib/client/Display.js';
 
-console.log("hey2");
+console.log('test');
 
-let sketch = (s) => {
-	s.setup = () =>{
-		s.createCanvas(window.innerWidth,window.innerHeight);
-    }
-    s.draw = () =>{
-		s.background(220);
-    }
-}
+let game = new Game();
+game.init();
+let dis = new Display();
+dis.init();
 
-const P5 = new p5(sketch);
+dis.stage = {
+		'rects': [
+		{
+			x: 10,
+			y: 10,
+			size: 100,
+			color: [0, 255, 0]
+		},
+		{
+			x: 50,
+			y: 50,
+			size: 125,
+			color: [255, 255, 0]
+		},
+		{
+			x: 100,
+			y: 100,
+			size: 300,
+			color: [0, 255, 255]
+		}
+	]
+};
+
+console.log(game, dis.backgroundColor);
+
+dis.backgroundColor = 100;
+
+// const P5 = new p5(sketch);
 
