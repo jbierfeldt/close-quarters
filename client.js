cc8a63469c92834124c1d228dbdb195201b579ef
Jackson Bierfeldt <jbierfeldt@gmail.com>
2020-06-12 23:37:27 -0400

    Updated Game Engine with movement

--- client.js
+++ client.js
@@ -2,15 +2,34 @@
 import Game from './lib/shared/Game.js';
 import Display from './lib/client/Display.js';
 import * as Units from './lib/shared/Unit.js';
+import {DEBUG} from './lib/shared/utilities.js';
+
+window.debug = new DEBUG(true, 0);
 
 let game = new Game();
 game.init();
 let dis = new Display();
 dis.init();
 
+
 const ray1 = new Units.RayTracer(100, 75, game.players[1]);
 const ray2 = new Units.RayTracer(0, null, game.players[1]);
-console.log(ray1, ray2);
+
+// game.registerGameObject(ray1);
+// game.registerGameObject(ray2);
+
+game.addObjectAtCoord(ray1, 2, 2);
+game.registerGameObject(ray1);
+debug.log(0, JSON.parse(JSON.stringify(game.board)));
+
+// game.removeObjectAtCoord(ray1, 2, 2);
+// // debug.log(0, JSON.parse(JSON.stringify(game.board)));
+
+// game.moveObject(ray1, 2, 2, 2, 3);
+// debug.log(0, JSON.parse(JSON.stringify(game.board)));
+
+
+game.runSimulation();
 
 // put board on grid
 
