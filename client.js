df04c10138a8268b1e66ba1decd22108d7a65f46
Jackson Bierfeldt <jbierfeldt@gmail.com>
2020-06-12 20:44:58 -0400

    Merged Unit.js / Added Projectile.JS
    - Changed id to generate uniuqely
    - Removed "unique identifier" as this won't be needed

--- client.js
+++ client.js
@@ -1,34 +1,16 @@
 // import p5 from 'p5';
 import Game from './lib/shared/Game.js';
 import Display from './lib/client/Display.js';
+import * as Units from './lib/shared/Unit.js';
 
 let game = new Game();
 game.init();
 let dis = new Display();
 dis.init();
 
-dis.stage = {
-		'rects': [
-		{
-			x: 10,
-			y: 10,
-			size: 100,
-			color: [0, 255, 0]
-		},
-		{
-			x: 50,
-			y: 50,
-			size: 125,
-			color: [255, 255, 0]
-		},
-		{
-			x: 100,
-			y: 100,
-			size: 300,
-			color: [0, 255, 255]
-		}
-	]
-};
+const ray1 = new Units.RayTracer(100, 75, game.players[1]);
+const ray2 = new Units.RayTracer(0, null, game.players[1]);
+console.log(ray1, ray2);
 
 // put board on grid
 
