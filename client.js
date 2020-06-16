3fcea435583da4229a357e7d6d3e526500e5d386
Jackson Bierfeldt <jbierfeldt@gmail.com>
2020-06-15 23:30:47 -0400

    Fixed Serialization Bug

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ import {DEBUG} from './lib/shared/utilities.js';
 
 
 
-window.debug = new DEBUG(true, 0);
+window.debug = new DEBUG(true, 2);
 
 
 class App {
@@ -14,6 +14,7 @@ class App {
 	constructor(game = new Game(), display = new Display(this)) {
 		this.game = game;
 		this.display = display;
+		this.gameState = undefined;
 	}
 
 	init() {
@@ -27,9 +28,18 @@ class App {
 		this.game.registerGameObject(oneUnit);
 		//debug.log(3,player)
 	}
+
 	appRunSimulation() {
 		this.game.runSimulation();
-		this.display.board = this.game.history.turn[this.game.turnNumber-1];
+		this.display.board = this.loadSerializedGameState(app.game.s_history.turn[this.game.turnNumber-1]);
+	}
+
+	loadSerializedGameState(serializedGameState) {
+		let tickContainer = this.game.loadSerializedGameState(serializedGameState);
+		for (const [key, value] of Object.entries(tickContainer.tick)) {
+  			tickContainer.tick[key] = JSON.parse(value);
+		}
+		return tickContainer;
 	}
 
 }
@@ -37,30 +47,30 @@ class App {
 const app = new App();
 app.init();
 
-const ray1 = new Units.RayTracer(100, 75, 1);
-const ray2 = new Units.RayTracer(0, 0, 1);
-const mag1 = new Units.Maglev(10, 10, 1);
+
+
+// const ray1 = new Units.RayTracer(100, 75, 1);
+// const ray2 = new Units.RayTracer(0, 0, 1);
+// const mag1 = new Units.Maglev(10, 10, 1);
 
 
 // turn 1 begins
 
-app.game.addObjectAtCoord(ray1, 2, 2);
-app.game.registerGameObject(ray1);
+// app.game.addObjectAtCoord(ray1, 2, 2);
+// app.game.registerGameObject(ray1);
 
-app.game.runSimulation();
+// app.game.runSimulation();
 
-// turn 2 beings
+// // turn 2 beings
 
-app.game.addObjectAtCoord(ray2, 3, 6);
-app.game.registerGameObject(ray2);
+// app.game.addObjectAtCoord(ray2, 3, 6);
+// app.game.registerGameObject(ray2);
 
-app.game.addObjectAtCoord(mag1, 10, 10);
-app.game.registerGameObject(mag1);
+// app.game.addObjectAtCoord(mag1, 10, 10);
+// app.game.registerGameObject(mag1);
 
 
-app.game.runSimulation();*/
-
-// debug.log(2, ray1.serialize());
+// app.game.runSimulation();
 
 
 // log history for turns 1 and 2
@@ -78,7 +88,7 @@ app.game.runSimulation();*/
 app.display.stage.grid = app.game.board;
 //dis.phase = game.gamePhase;
 app.display.unitList = app.game.gameUnitList;
-app.display.board = app.game.loadSerializedGameState(app.game.s_history.turn[1]);
+// app.display.board = app.game.loadSerializedGameState(app.game.s_history.turn[1]);
 
 
 // const P5 = new p5(sketch);
