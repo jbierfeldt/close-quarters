f1bd35f94fd0978c18f1eef6d68922fb66b07a4b
Jackson Bierfeldt <jbierfeldt@gmail.com>
2020-06-15 22:21:18 -0400

    Serialize History

--- client.js
+++ client.js
@@ -30,9 +30,6 @@ class App {
 const app = new App();
 app.init();
 
-debug.log(2, app.game.board[19][30]);
-
-
 const ray1 = new Units.RayTracer(100, 75, 1);
 const ray2 = new Units.RayTracer(0, 0, 1);
 const ray3 = new Units.RayTracer(0, 0, 2);
@@ -53,13 +50,15 @@ app.game.registerGameObject(ray2);
 
 app.game.runSimulation();
 
-debug.log(2, ray1.serialize());
+// debug.log(2, ray1.serialize());
 
 
 // log history for turns 1 and 2
 
-debug.log(0, app.game.history);
-
+// debug.log(2, app.game.history);
+// debug.log(2, app.game.s_history);
+// debug.log(2, app.game.s_history.turn[1]);
+// debug.log(2, app.game.loadSerializedGameState(app.game.s_history.turn[2]));
 
 
 
@@ -69,7 +68,7 @@ debug.log(0, app.game.history);
 app.display.stage.grid = app.game.board;
 //dis.phase = game.gamePhase;
 app.display.unitList = app.game.gameUnitList;
-app.display.board = app.game.history.turn[2];
+app.display.board = app.game.loadSerializedGameState(app.game.s_history.turn[1]);
 
 
 // const P5 = new p5(sketch);
