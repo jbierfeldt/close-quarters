f2beea2bc75ffc03c1be23795b02c90e17356969
Jackson Bierfeldt <jbierfeldt@gmail.com>
2020-09-08 20:05:53 -0400

    Refactored File Structure

---
+++ src/lib/server/ClientController.js
@@ -0,0 +1,180 @@
+import {DEBUG, createID} from '../shared/utilities.js';
+const debug = new DEBUG(process.env.DEBUG, 0);
+
+export default class ClientController {
+
+	constructor({id, token}) {
+		this.id = id; // UUID for client, using UUID from first socket
+		this.token = token; // signed auth token
+
+    this.socket = null;
+		this.gameController = null;
+		this.playerNumber = null;
+
+		this.isConnected = false;
+
+		this.ordersToExecute = [];
+		this.ordersSubmitted = false;
+
+		this.clientGamePhase = null;
+		this.clientState = null; // null, SPECTATOR, ACTIVE_PLAYER, DEFEATED_PLAYER
+	}
+
+	init() {
+	}
+
+	onConnect () {
+		this.sendClientInfo();
+		this.socket.emit("debugInfoUpdate");
+	}
+
+	setSocket (socket) {
+		if (this.socket !== null) {
+			this.removeListeners();
+			this.socket.disconnect();
+		};
+
+		this.socket = socket;
+
+		this.bindListeners();
+	}
+
+  setGameController (gameController) {
+    if (this.gameController !== null) {
+      this.removeListeners();
+      // this.gameController.unsubscribe(this) ? something like this?
+    }
+
+    this.gameController = gameController;
+    this.bindGameListeners();
+  }
+
+	removeListeners () {
+		this.socket.removeAllListeners();
+	}
+
+  bindListeners () {
+    this.socket.on('connection', () => {
+			console.log('client ' + this.id + 'connect');
+		})
+  }
+
+	bindGameListeners () {
+
+		this.socket.on('connection', () => {
+			console.log('client ' + this.id + 'connect');
+		})
+
+		this.socket.on('createUnit', (data) => {
+			debug.log(0, `Received createUnit from ${this.socket.id}`);
+			this.gameController.createUnit(data.unitType, data.player, data.x, data.y);
+		});
+
+		this.socket.on('createBase', (data) => {
+			this.gameController.createBase(data.baseType, data.player, data.x, data.y);
+		});
+
+		this.socket.on('submitTurn', (data) =>  {
+			debug.log(0, `Received submit turn with ${data}`);
+      this.eventHandler.publish('turnSubmitted', data);
+			// will update game controller saying that this player has submitted their turn
+			// for now, just forcing runSimulation
+			this.ordersToExecute = JSON.parse(data);
+			this.ordersSubmitted = true;
+
+			// try to execute orders
+			this.gameController.checkAllOrdersSubmitted();
+		});
+
+		this.socket.on('forcesubmitTurn', (data) =>  {
+			// will update game controller saying that this player has submitted their turn
+			// for now, just forcing runSimulation
+			this.ordersToExecute = JSON.parse(data);
+			this.ordersSubmitted = true;
+
+			// try to execute orders
+			this.gameController.forceOrders();
+		});
+
+		this.socket.on('printServerData', () => {
+      this.eventHandler.publish('printServerData');
+			this.gameController.printServerData();
+		});
+
+		this.socket.on('resetGame', (data) =>  {
+			// reset Game
+      this.eventHandler.publish('resetGame', data);
+			this.gameController.resetGame();
+		});
+
+		this.socket.on('loadSnapshot', (data) => {
+			debug.log(0, `Got loadSnapshot`);
+      this.eventHandler.publish('loadSnapshot', data);
+
+			try {
+        let snapshot = JSON.parse(data);
+				if (snapshot.turnNumber && snapshot.currentTurnInitialState) {
+					this.gameController.loadSnapshot(snapshot);
+				}
+				return true;
+    	} catch(e) {
+				debug.log(0, e);
+				return false;
+    	}
+
+		})
+
+		this.socket.on('updateClientPhase', (data) => {
+			this.clientGamePhase = data.newPhase;
+      this.eventHandler.publish('updateClientPhase', data);
+			this.gameController.sendServerStateToAll();
+		})
+
+		this.socket.on('disconnect', (reason) => {
+			console.log("disconnected", this.id, this.socket.id, reason);
+      this.eventHandler.publish('disconnected', reason);
+			this.gameController.clientControllerDisconnect(this);
+			this.gameController.sendServerStateToAll();
+
+			// check if all orders have now been submitted
+			this.gameController.checkAllOrdersSubmitted();
+		});
+
+	}
+
+	setClientStateFromVictoryCondition (victoryCondition) {
+		let newState;
+		switch (victoryCondition) {
+			case -1:
+				newState = "DEFEATED_PLAYER";
+				break;
+			case 0:
+				newState = "ACTIVE_PLAYER";
+				break;
+			case 1:
+				newState = "VICTORIOUS_PLAYER";
+		}
+
+		if (this.clientState !== newState) {
+			this.clientState = newState;
+			this.sendClientState();
+		}
+	}
+
+	sendClientState () {
+		console.log("sending client state", this.id, this.clientState);
+		this.socket.emit("updateClientState", {
+			'clientState': this.clientState
+		});
+	}
+
+	sendClientInfo () {
+		this.socket.emit("updateClientInfo", {
+			'clientID': this.id,
+			'token': this.token,
+			'clientState': this.clientState,
+			'playerNumber': this.playerNumber
+		});
+	}
+
+}
---
+++ src/lib/server/ConnectionHandler.js
@@ -0,0 +1,169 @@
+import {DEBUG, createID} from '../shared/utilities.js';
+const debug = new DEBUG(process.env.DEBUG, 0);
+import ClientController from './ClientController.js';
+import GameController from './GameController.js';
+
+const jwt = require('jsonwebtoken');
+
+class PubSub {
+
+  constructor () {
+    this.subscribers = {};
+  }
+
+  subscribe (event, callback) {
+    let index;
+    const that = this;
+
+    if (!this.subscribers[event]) {
+      this.subscribers[event] = [];
+    }
+
+    index = this.subscribers[event].push(callback) - 1;
+
+    return {
+      unsubscribe () {
+        that.subscribers[event].splice(index, 1);
+      }
+    }
+  }
+
+  publish (event, data) {
+    if (!this.subscribers[event]) return;
+
+    this.subscribers[event].forEach(subscriberCallback => {
+      subscriberCallback(data);
+    });
+  }
+
+ }
+
+export default class ConnectionHandler {
+
+	constructor (IO_INSTANCE, SECRET_KEY) {
+		this.io = IO_INSTANCE;
+		this.SECRET_KEY = SECRET_KEY;
+    this.events = new PubSub();
+
+    // current open game (temp)
+    this.openGame = null;
+
+		this.clientControllers = new Map();
+		this.gameControllers = new Map();
+	}
+
+	init () {
+
+		this.registerMiddleware();
+
+    let newGame = new GameController(this.io);
+    newGame.init();
+
+    this.gameControllers.set(newGame.id, newGame);
+
+    this.openGame = newGame;
+
+		this.io.on('connection', (socket) => {
+
+			// check for already existing token and if so, don't make new player controller
+			if (socket.sessionID) {
+				let clientController = this.clientControllers.get(socket.sessionID);
+
+				if (clientController) {
+					this.reconnectClientController(clientController, socket);
+				} else {
+					this.createClientController(socket);
+				}
+			} else {
+				this.createClientController(socket);
+			}
+
+			socket.clientController.onConnect();
+
+			debug.log(0, `new socket connected as ${socket.clientController.id}`);
+		});
+
+	}
+
+	registerMiddleware () {
+
+		// decode authToken using session specific secret, if set
+		// put decoded authToken in socket.sessionID
+		this.io.use((socket, next) => {
+			if (socket.handshake.query && socket.handshake.query.token && socket.handshake.query.token !== '') {
+				jwt.verify(socket.handshake.query.token, this.SECRET_KEY, (err, decoded) => {
+					if (err) return next();
+					socket.sessionID = decoded;
+					next();
+				});
+			}
+			else {
+				next();
+			}
+		});
+
+	}
+
+	signClientToken (clientUUID) {
+		const token = jwt.sign(clientUUID, this.SECRET_KEY);
+		return token;
+	}
+
+  bindGameControllerListeners (clientController, gameController) {
+    this.events.subscribe('turnSubmitted', (data) => {
+      console.log(`${clientController.id} has submitted turn with data ${data}`);
+    });
+
+    this.events.subscribe('printServerData', () => {
+      console.log(`${clientController.id} wants to print server data`);
+    });
+
+    this.events.subscribe('resetGame', () => {
+      console.log(`${clientController.id} wants to reset the game`);
+    });
+
+    this.events.subscribe('loadSnapshot', (data) => {
+      console.log(`${clientController.id} wants to load the snapshot ${data}`);
+    });
+
+    this.events.subscribe('updateClientPhase', (data) => {
+      console.log(`${clientController.id} has updated its clientPhase to be ${data}`);
+    });
+
+    this.events.subscribe('disconnected', (data) => {
+      console.log(`${clientController.id} has disconnected for reason: ${data}`);
+    });
+  }
+
+	// create new clientController with information from socket
+	createClientController (socket) {
+
+		let newClientController = new ClientController({
+			id: socket.id,
+			token: this.signClientToken(socket.id)
+		})
+
+    newClientController.eventHandler = this.events;
+
+		newClientController.setSocket(socket);
+
+    newClientController.setGameController(this.openGame);
+    this.bindGameControllerListeners(newClientController, this.openGame);
+    newClientController.playerNumber = 1;
+
+		socket.clientController = newClientController;
+
+		this.clientControllers.set(newClientController.id, newClientController);
+
+		console.log(this.clientControllers);
+
+	}
+
+	reconnectClientController (clientController, socket) {
+
+		clientController.setSocket(socket);
+
+		socket.clientController = clientController;
+
+	}
+}
